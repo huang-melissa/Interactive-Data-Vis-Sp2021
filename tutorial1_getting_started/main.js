6f872453dd33b8199084c15a7f5afa6d2f157d02
huang-melissa <mhuang.teaching@gmail.com>
2021-02-02 23:45:02 -0500

    updated

--- tutorial1_getting_started/main.js
+++ tutorial1_getting_started/main.js
@@ -1,2 +1,7 @@
 console.log("This is going to make a pop-up alert.")
-alert("Welcome!")
+/* this is how you comment 
+you can comment multiple lines 
+*/ 
+let message;
+message = "Welcome!"; // store the string
+alert(message); // create alert with variable content
