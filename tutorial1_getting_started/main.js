5b4e5060d6d30a319169ebd449e48515373d82c5
huang-melissa <mhuang.teaching@gmail.com>
2021-02-03 21:23:19 -0500

    update js

--- tutorial1_getting_started/main.js
+++ tutorial1_getting_started/main.js
@@ -1,7 +1,7 @@
+let message;
+message = "Welcome to my deployed example for Tutorial 1!"; // store the string
+alert(message); // create alert with variable content
 console.log("This is going to make a pop-up alert.")
 /* this is how you comment 
 you can comment multiple lines 
 */ 
-let message;
-message = "Welcome to my deployed example for Tutorial 1!"; // store the string
-alert(message); // create alert with variable content
